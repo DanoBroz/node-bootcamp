edeef0d48c0cd8f8ffac7a5f1050c38ef20f8e83
Daniel Brož <danobroz66@gmail.com>
2022-10-06 14:46:03 +0200

    perform data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
@@ -31,6 +33,12 @@ app.use('/api', limiter)
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }))
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// data sanitization against XSS
+app.use(xss())
 
 // serving static files
 app.use(express.static(`${__dirname}/public`))
