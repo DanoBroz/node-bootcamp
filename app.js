d57d5e7a3865beff9fd668e549d05692348b14fc
Daniel Brož <danobroz66@gmail.com>
2022-10-06 14:07:44 +0200

    add security http headers and limit body to 10kb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -10,10 +11,15 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 // global Middlewares
+// security http headers
+app.use(helmet())
+
+// development loggin
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// limit request from same api
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -23,9 +29,13 @@ const limiter = rateLimit({
 
 app.use('/api', limiter)
 
-app.use(express.json())
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }))
+
+// serving static files
 app.use(express.static(`${__dirname}/public`))
 
+// test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
     console.log(req.headers)
