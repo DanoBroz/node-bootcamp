5da0b79515420d2ceda891c7634fd6275b87c10e
Daniel Brož <danobroz66@gmail.com>
2022-10-06 13:40:58 +0200

    create limitter for number of log in

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
-// Middlewares
+// global Middlewares
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message:
+        'Too many requests from this IP, please try again in an hour!',
+})
+
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
