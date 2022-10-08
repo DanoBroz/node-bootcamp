eb8a457fb2fa550e2adce6910afab4325da836eb
Daniel Brož <danobroz66@gmail.com>
2022-10-08 17:31:22 +0200

    create review routes controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,32 @@
+const catchAsync = require('../utils/catchAsync')
+const Review = require('../models/reviewModel')
+
+exports.getAllReviews = catchAsync(
+    async (req, res, next) => {
+        const reviews = await Review.find()
+
+        console.log(req.query)
+        res.status(200).json({
+            status: 'success',
+            length: reviews.length,
+            data: {
+                reviews,
+            },
+        })
+    }
+)
+
+exports.createReview = catchAsync(
+    async (req, res, next) => {
+        const newReview = await Review.create(
+            req.body
+        )
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                review: newReview,
+            },
+        })
+    }
+)
