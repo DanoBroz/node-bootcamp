8b31b277a0b8713001ca039423e149bb4d930f88
Daniel Brož <danobroz66@gmail.com>
2022-10-08 17:50:07 +0200

    seletect populated values of reviewed user and tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -43,7 +43,13 @@ const reviewSchema = new mongoose.Schema(
 )
 
 reviewSchema.pre(/^find/, function (next) {
-    this.populate('tour user')
+    this.populate({
+        path: 'tour',
+        select: 'name',
+    }).populate({
+        path: 'user',
+        select: 'name photo',
+    })
     next()
 })
 
