b777b781ae549da6772317de065734350b77279d
Daniel Brož <danobroz66@gmail.com>
2022-10-12 19:35:02 +0200

    calculate ratings when review deleted or updated

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Tour = require('./tourModel')
 
 const reviewSchema = new mongoose.Schema(
     {
@@ -75,12 +76,44 @@ reviewSchema.statics.calcAverageRatings =
             },
         ])
         console.log(stats)
+
+        if (stats.length > 0) {
+            await Tour.findByIdAndUpdate(tourId, {
+                ratingsQuantity: stats[0].nRating,
+                ratingsAverage:
+                    stats[0].avgRating,
+            })
+        } else {
+            await Tour.findByIdAndUpdate(tourId, {
+                ratingsQuantity: 0,
+                ratingsAverage: 4.5,
+            })
+        }
     }
 
 reviewSchema.post('save', function () {
     // this points to current review
     this.constructor.calcAverageRatings(this.tour)
 })
+
+reviewSchema.pre(
+    /^findOneAnd/,
+    async function (next) {
+        // get the document after query has been called
+        this.r = await this.findOne()
+        console.log(this.r)
+    }
+)
+
+reviewSchema.post(
+    /^findOneAnd/,
+    async function () {
+        // await this.findOne(); does NOT work here, query has already executed
+        await this.r.constructor.calcAverageRatings(
+            this.r.tour
+        )
+    }
+)
 
 const Review = mongoose.model(
     'Review',
