bcbed4849f49e61b5744b0255a5686c2ca35d235
Daniel Brož <danobroz66@gmail.com>
2022-10-13 18:23:15 +0200

    get first aggregate parametr for geolocation to geoNear

--- models/tourModel.js
+++ models/tourModel.js
@@ -215,12 +215,12 @@ tourSchema.pre(/^find/, function (next) {
     next()
 })
 
-tourSchema.pre('aggregate', function (next) {
-    this.pipeline().unshift({
-        $match: { secretTour: { $ne: true } },
-    })
-    next()
-})
+// tourSchema.pre('aggregate', function (next) {
+//     this.pipeline().unshift({
+//         $match: { secretTour: { $ne: true } },
+//     })
+//     next()
+// })
 
 tourSchema.post(/^find/, function (docs, next) {
     console.log(
