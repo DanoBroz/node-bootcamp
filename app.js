756db49f6856c1424a5802e783134fdd08b31e3f
Daniel Brož <danobroz66@gmail.com>
2022-10-16 15:11:06 +0200

    use pug template in views folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
@@ -14,8 +15,16 @@ const reviewRouter = require('./routes/reviewRoutes')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
 // global Middlewares
 // security http headers
+
+// serving static files
+app.use(
+    express.static(path.join(__dirname, 'public'))
+)
 app.use(helmet())
 
 // development loggin
@@ -56,9 +65,6 @@ app.use(
     })
 )
 
-// serving static files
-app.use(express.static(`${__dirname}/public`))
-
 // test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString()
@@ -67,6 +73,10 @@ app.use((req, res, next) => {
 })
 
 // Routes
+app.get('/', (req, res) => {
+    res.status(200).render('base')
+})
+
 app.use('/api/v1/tours/', tourRouter)
 app.use('/api/v1/users/', userRouter)
 app.use('/api/v1/reviews/', reviewRouter)
