3b4913768577976bde7da6603d3eb0c644605971
Daniel Brož <danobroz66@gmail.com>
2022-10-18 16:26:58 +0200

    get tour based on slug parametr

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -16,8 +16,16 @@ exports.getOverview = catchAsync(
     }
 )
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+    const tour = await Tour.findOne({
+        slug: req.params.slug,
+    }).populate({
+        path: 'reviews',
+        fields: 'review rating user',
+    })
+
     res.status(200).render('tour', {
         title: 'The forest hiker tour',
+        tour,
     })
-}
+})
