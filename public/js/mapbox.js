4287e443593d801cacf7e3fc5bca018ff558655e
Daniel Brož <danobroz66@gmail.com>
2022-10-28 07:27:30 +0200

    show map on tour template

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -7,3 +7,10 @@ const locations = JSON.parse(
 )
 
 console.log(locations)
+
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiYnJvZGFuIiwiYSI6ImNsOXI4MnpmZDAzOGkzeW84c3N6Z3kzaWgifQ.Ys7KL1wVQFEWcxDnNQYjNw'
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+})
