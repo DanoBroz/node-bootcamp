e749f6be034afd35ec8ea300e3e1a28397205b61
Daniel Brož <danobroz66@gmail.com>
2022-10-31 14:01:34 +0100

    use login middleware on viewRoutes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const viewsController = require('../controllers/viewsController')
+const authController = require('../controllers/authController')
 
 const router = express.Router()
+
+// use log in middleware
+router.use(authController.isLoggedIn)
 
 router.get('/', viewsController.getOverview)
 
