899327223fb90f20626fe2b9f6fc2dd9a2d63748
Daniel Brož <danobroz66@gmail.com>
2022-10-31 17:46:58 +0100

    create logout route handler

--- public/js/login.js
+++ public/js/login.js
@@ -29,3 +29,18 @@ export const login = async (email, password) => {
         )
     }
 }
+
+export const logout = async () => {
+    try {
+        const res = await axios.get(
+            'http://localhost:3000/api/v1/users/logout'
+        )
+        if (res.data.status === 'success')
+            location.reload(true)
+    } catch (err) {
+        showAlert(
+            'error',
+            'Error logging out! Try again.'
+        )
+    }
+}
