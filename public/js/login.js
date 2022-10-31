cd4b699205d5b18176a187cf0762fdf86a57e4b4
Daniel Brož <danobroz66@gmail.com>
2022-10-31 17:22:37 +0100

    show alerts on successful and errorful login

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
+import '@babel/polyfill'
+import axios from 'axios'
+import { showAlert } from './alerts'
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
     try {
         const result = await axios.post(
             'http://localhost:3000/api/v1/users/login',
@@ -11,28 +14,18 @@ const login = async (email, password) => {
         )
 
         if (result.data.status === 'success') {
-            alert('Logged in successfully!')
+            showAlert(
+                'success',
+                'Logged in successfully!'
+            )
             window.setTimeout(() => {
                 location.assign('/')
             }, 1500)
         }
     } catch (err) {
-        alert(err.response.data.message)
+        showAlert(
+            'error',
+            err.response.data.message
+        )
     }
 }
-
-document
-    .querySelector('.form')
-    .addEventListener('submit', (e) => {
-        e.preventDefault()
-
-        const email =
-            document.getElementById('email').value
-
-        const password =
-            document.getElementById(
-                'password'
-            ).value
-
-        login(email, password)
-    })
