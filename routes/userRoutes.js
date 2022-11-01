db59fab3b545ed3326f8c1261e3634f0553497c2
Daniel Brož <danobroz66@gmail.com>
2022-11-01 18:01:16 +0100

    use upload multer middleware for update request

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const multer = require('multer')
 const userController = require('../controllers/userController')
 const authController = require('../controllers/authController')
+
+const upload = multer({
+    dest: 'public/img/users',
+})
 
 const router = express.Router()
 
@@ -30,7 +35,11 @@ router.patch(
     '/updateMyPassword',
     authController.updatePassword
 )
-router.patch('/updateMe', userController.updateMe)
+router.patch(
+    '/updateMe',
+    upload.single('photo'),
+    userController.updateMe
+)
 router.delete(
     '/deleteMe',
     userController.deleteMe
