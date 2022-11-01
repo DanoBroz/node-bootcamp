be526fce20c41fc553e5c0bc541a19b1327b2e70
Daniel Brož <danobroz66@gmail.com>
2022-11-01 12:43:53 +0100

    catch an error when searched for bad tour

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel')
+const AppError = require('../utils/appError')
 const catchAsync = require('../utils/catchAsync')
 
 exports.getOverview = catchAsync(
@@ -16,19 +17,30 @@ exports.getOverview = catchAsync(
     }
 )
 
-exports.getTour = catchAsync(async (req, res) => {
-    const tour = await Tour.findOne({
-        slug: req.params.slug,
-    }).populate({
-        path: 'reviews',
-        fields: 'review rating user',
-    })
+exports.getTour = catchAsync(
+    async (req, res, next) => {
+        const tour = await Tour.findOne({
+            slug: req.params.slug,
+        }).populate({
+            path: 'reviews',
+            fields: 'review rating user',
+        })
 
-    res.status(200).render('tour', {
-        title: `${tour.name} Tour`,
-        tour,
-    })
-})
+        if (!tour) {
+            return next(
+                new AppError(
+                    'There is no tour with that name.',
+                    404
+                )
+            )
+        }
+
+        res.status(200).render('tour', {
+            title: `${tour.name} Tour`,
+            tour,
+        })
+    }
+)
 
 exports.getLogin = catchAsync(
     async (req, res) => {
