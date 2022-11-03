9325b80976bb211facc64137fb5be61f02ed557a
Daniel Brož <danobroz66@gmail.com>
2022-11-03 09:13:25 +0100

    add booking crud operations

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -89,3 +89,9 @@ exports.createBookingCheckout = catchAsync(
         )
     }
 )
+
+exports.createBooking = factory.createOne(Booking)
+exports.getBooking = factory.getOne(Booking)
+exports.getAllBookings = factory.getAll(Booking)
+exports.updateBooking = factory.updateOne(Booking)
+exports.deleteBooking = factory.deleteOne(Booking)
