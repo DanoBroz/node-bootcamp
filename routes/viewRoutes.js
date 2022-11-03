3f67a0f2b684b5a8d1487b623201b8b88194567d
Daniel Brož <danobroz66@gmail.com>
2022-11-03 08:41:54 +0100

    use create booking middleware in view routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const viewsController = require('../controllers/viewsController')
 const authController = require('../controllers/authController')
+const bookingController = require('../controllers/bookingController')
 
 const router = express.Router()
 
 // use log in middleware
 router.get(
     '/',
+    bookingController.createBookingCheckout,
     authController.isLoggedIn,
     viewsController.getOverview
 )
